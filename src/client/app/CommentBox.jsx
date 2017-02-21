10b8036700900fe1b8e0c4406a86e188b30c0bdb
pashashiz <pogrebnij@gmail.com>
2017-02-21 23:30:08 +0200

    Baseline

---
+++ src/client/app/CommentBox.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CommentList from './CommentList.jsx';
+import CommentForm from './CommentForm.jsx';
+
+export default class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+}
---
+++ src/client/app/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import {render} from 'react-dom';
+import CommentBox from './CommentBox.jsx';
+
+render(<CommentBox/>, document.getElementById('content'));
