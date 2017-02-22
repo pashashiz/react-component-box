617e81f079048f4f7bf6d08fd2f8f4731cbce6a9
pashashiz <pogrebnij@gmail.com>
2017-02-23 01:01:14 +0200

    Added data loading

--- src/client/app/CommentBox.jsx
+++ src/client/app/CommentBox.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import CommentList from './CommentList.jsx';
 import CommentForm from './CommentForm.jsx';
+import $ from 'jquery';
 
 export default class CommentBox extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {data: []};
+    }
+
+    componentDidMount() {
+        this.loadCommentsFromServer();
+        let component = this;
+        setInterval(() => {
+            component.loadCommentsFromServer();
+        }, this.props.pollInterval);
+    }
+
+    loadCommentsFromServer() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: (data) => {
+                this.setState({data: data});
+            },
+            error: (xhr, status, err) => {
+                console.error(this.props.url, status, err.toString());
+            }
+        });
+    }
 
     render() {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList data={this.props.data}/>
+                <CommentList data={this.state.data}/>
                 <CommentForm />
             </div>
         );
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -2,9 +2,4 @@ import React from 'react';
 import {render} from 'react-dom';
 import CommentBox from './CommentBox.jsx';
 
-let data = [
-    {id: 1, author: "Pete Hunt", text: "This is one comment"},
-    {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
-
-render(<CommentBox data={data}/>, document.getElementById('content'));
+render(<CommentBox url="/api/comments" pollInterval={2000} />, document.getElementById('content'));
